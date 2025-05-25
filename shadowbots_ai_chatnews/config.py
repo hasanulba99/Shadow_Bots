48532d23327eadfb48d14a5a3780e535099ffd4a
hasanulba99 <hasanulba019@gmail.com>
2025-05-25 23:15:19 +0600

    Update config.py

--- shadowbots_ai_chatnews/config.py
+++ shadowbots_ai_chatnews/config.py
@@ -1,2 +1,2 @@
-NEWS_API_KEY = "your_newsapi_key_here"
-OPENAI_API_KEY = "your_openai_api_key_here"
+NEWS_API_KEY = "26219ba7818d4227b0b65211e98d619a"
+OPENAI_API_KEY = "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"
